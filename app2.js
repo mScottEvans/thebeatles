a70421bfec78415ced068a5e0a83f14737002dd9
Scott Evans <michealsevans20@gmail.com>
2021-09-02 16:01:41 -0500

    added that

--- app2.js
+++
@@ -1,9 +0,0 @@
-let userAnswer = prompt('Is The Beatles your favorite band?')
-console.log(userAnswer)
-if(userAnswer.toLowerCase() == 'yes'){
-    let url = "https://cdn0.vox-cdn.com/thumbor/JTrP7wxDVveVMaPRxl6qcY_okvQ=/0x21:1240x719/1600x900/cdn0.vox-cdn.com/uploads/chorus_image/image/48424833/beatles.0.0.jpg"
-    document.write('<img src="' + url + '" id="thebeatles">')
-} else{
-    let url2 = "https://tse3.mm.bing.net/th?id=OIP.8xJM25IHPWrFcW_WecIcUQAAAA&pid=Api&P=0&w=300&h=300"
-    document.write('<img src="' + url2 + '" id="thebeatles2">')
-}
